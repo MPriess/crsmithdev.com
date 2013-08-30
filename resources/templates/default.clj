c64a30faa1033347b806a99cf416b51e422dad75
Chris Smith <chris@cir.ca>
2013-08-30 08:11:17 -0700

    testing disqus

--- resources/templates/default.clj
+++ resources/templates/default.clj
@@ -38,7 +38,17 @@
     (if (= (:type metadata) :post)
       [:div.row
        [:div.col-md-12
-        content]]
+        content
+        [:div.disqus_thread]
+        [:script {:type "text/javascript"}
+         "var disqus_shortname = 'crsmithdev';"
+         "(function() {"
+            "var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;"
+            "var disqus_identifier = 'http://crsmithdev.github.com/blog/2013/03/26/building-better-email-habits-with-mailbox/';"
+            "var disqus_url = 'http://crsmithdev.github.com/blog/2013/03/26/building-better-email-habits-with-mailbox/';"
+            "dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';"
+            "(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);"
+         "})();"]]]
       content)
     [:script {:src "//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"}]
     [:script {:src "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.0-rc2/js/bootstrap.min.js"}]
