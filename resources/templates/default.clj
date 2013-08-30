7c7c12634c493887a9f0313c723c2f2b15273b29
Chris Smith <chris@cir.ca>
2013-08-30 08:26:13 -0700

    google analytics tracking added

--- resources/templates/default.clj
+++ resources/templates/default.clj
@@ -53,7 +53,14 @@
     [:script {:src "//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"}]
     [:script {:src "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.0-rc2/js/bootstrap.min.js"}]
     [:script {:src "//cdnjs.cloudflare.com/ajax/libs/highlight.js/7.3/highlight.min.js"}]
-    [:script {:src "/js/crsmithdev.js"}]]]
+    [:script {:src "/js/crsmithdev.js"}]
+    [:script {:type "text/javascript"}
+     "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){"
+     "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),"
+     "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)"
+     "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');"
+     "ga('create', 'UA-40826256-2', 'crsmithdev.com');"
+     "ga('send', 'pageview');"]]]
   [:div.footer
    [:div.container
     [:div.row
